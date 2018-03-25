1de31b9f2321d84ba42e1c3319e2142917794fe9
amit dash <dbamit@thoughtworks.com>
2018-03-25 20:00:35 +0530

    Added borrowing sample

--- src/main.rs
+++ src/main.rs
@@ -30,3 +30,15 @@ fn which_is_greater<'a, 'b>(a: &'a i32, b: &'b i32) -> &'b i32 {
 //    }
     &(10)
 }
+
+fn test_mut_ref() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    let mut vec1: Vec<i32> = Vec::new();
+}
+
+fn copy_vec(from: &Vec<i32>, to: &mut Vec<i32>) {
+    for elem in from {
+        to.push(*elem);
+    }
+}
